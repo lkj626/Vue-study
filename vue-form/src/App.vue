e544be0eece481068c19417f4d4de12b129da990
이강진 <hwi2337@naver.com>
2022-09-06 22:28:46 +0900

    Vue study and setting

---
+++ vue-form/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+   <div>
+    <form action="">
+      <div>
+        <label for="username">ID: </label>
+        <input id="username" type="text">
+      </div>
+      <div>
+        <label for="password">PW: </label>
+        <input id="password" type="password">  
+      </div>
+    </form>
+
+   </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
